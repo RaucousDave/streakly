6b419ae96e0a4f8a6d9596b1f4c55928ad4f155f
RaucousDave <daviesenienghan906@gmail.com>
2026-08-24 12:25:04 +0100

    fix: remove server listening function

--- server/index.ts
+++ server/index.ts
@@ -52,7 +52,4 @@ app.get("/api/health", (_req, res) => {
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000....")
-})
 export default app;
